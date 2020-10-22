4448ab2047b8846591e93c3aafd6d5232ea93b43
oberlt2922 <53795023+oberlt2922@users.noreply.github.com>
2020-10-22 14:05:47 -0400

    Add files via upload

---
+++ MineSweeper3/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace MineSweeper3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.easyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mediumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxFlags = new System.Windows.Forms.TextBox();
+            this.labelGameOver = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(527, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.easyToolStripMenuItem,
+            this.mediumToolStripMenuItem,
+            this.hardToolStripMenuItem});
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            // 
+            // easyToolStripMenuItem
+            // 
+            this.easyToolStripMenuItem.Name = "easyToolStripMenuItem";
+            this.easyToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.easyToolStripMenuItem.Text = "Easy";
+            this.easyToolStripMenuItem.Click += new System.EventHandler(this.easyToolStripMenuItem_Click);
+            // 
+            // mediumToolStripMenuItem
+            // 
+            this.mediumToolStripMenuItem.Name = "mediumToolStripMenuItem";
+            this.mediumToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.mediumToolStripMenuItem.Text = "Medium";
+            this.mediumToolStripMenuItem.Click += new System.EventHandler(this.easyToolStripMenuItem_Click);
+            // 
+            // hardToolStripMenuItem
+            // 
+            this.hardToolStripMenuItem.Name = "hardToolStripMenuItem";
+            this.hardToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.hardToolStripMenuItem.Text = "Hard";
+            this.hardToolStripMenuItem.Click += new System.EventHandler(this.easyToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(463, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Flags";
+            // 
+            // textBoxFlags
+            // 
+            this.textBoxFlags.Enabled = false;
+            this.textBoxFlags.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxFlags.Location = new System.Drawing.Point(467, 57);
+            this.textBoxFlags.Name = "textBoxFlags";
+            this.textBoxFlags.ReadOnly = true;
+            this.textBoxFlags.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBoxFlags.Size = new System.Drawing.Size(41, 26);
+            this.textBoxFlags.TabIndex = 2;
+            // 
+            // labelGameOver
+            // 
+            this.labelGameOver.AutoSize = true;
+            this.labelGameOver.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGameOver.ForeColor = System.Drawing.Color.Red;
+            this.labelGameOver.Location = new System.Drawing.Point(464, 99);
+            this.labelGameOver.Name = "labelGameOver";
+            this.labelGameOver.Size = new System.Drawing.Size(41, 13);
+            this.labelGameOver.TabIndex = 3;
+            this.labelGameOver.Text = "label2";
+            this.labelGameOver.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 445);
+            this.Controls.Add(this.labelGameOver);
+            this.Controls.Add(this.textBoxFlags);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Mine Sweeper";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem easyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mediumToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hardToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxFlags;
+        private System.Windows.Forms.Label labelGameOver;
+    }
+}
+
---
+++ MineSweeper3/Form1.cs
@@ -0,0 +1,434 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MineSweeper3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //change if statements so they are more readable
+                    //add variable for i + yNeighborArray[z] etc
+        //add method to set button background immage to number pictures
+
+
+        /////////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////Global Variables
+        bool gameOver;
+        int gridSize;
+        int mineLimit;
+        int flagLimit;
+        MineSweeperButton[,] btnArray;
+        int[] yNeighborArray = new int[8] { -1, -1, 0, 1, 1, 1, 0, -1 };
+        int[] xNeighborArray = new int[8] { 0, -1, -1, -1, 0, 1, 1, 1 };
+        ///////////////////////////////////////Global Variables
+        /////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////New Game
+        private void easyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem menuItem = (ToolStripMenuItem)sender;
+            switch (menuItem.Text)
+            {
+                case "Easy":
+                    {
+                        if (MessageBox.Show("Do you want to start a new game in" + menuItem.Text + " mode.",
+                        "New Game", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                        {
+                            ClearForm();
+                            gameOver = false;
+                            gridSize = 10;
+                            mineLimit = 15;
+                            flagLimit = 15;
+                            SetUpGrid();
+                            FillMines();
+                            FillNumbers();
+                        }
+                        break;
+                    }
+                case "Medium":
+                    {
+                        if (MessageBox.Show("Do you want to start a new game in" + menuItem.Text + " mode.",
+                        "New Game", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                        {
+                            ClearForm();
+                            gameOver = false;
+                            gridSize = 15;
+                            mineLimit = 40;
+                            flagLimit = 40;
+                            SetUpGrid();
+                            FillMines();
+                            FillNumbers();
+                        }
+                        break;
+                    }
+                case "Hard":
+                    {
+                        if (MessageBox.Show("Do you want to start a new game in" + menuItem.Text + " mode.",
+                        "New Game", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                        {
+                            ClearForm();
+                            gameOver = false;
+                            gridSize = 20;
+                            mineLimit = 80;
+                            flagLimit = 80;
+                            SetUpGrid();
+                            FillMines();
+                            FillNumbers();
+                        }
+                        break;
+                    }
+            }
+            textBoxFlags.Text = Convert.ToString(flagLimit);
+        }
+        ///////////////////////////////////////////New Game
+        ////////////////////////////////////////////////////////////////////////////////////////
+        
+
+
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////Clear form
+        private void ClearForm()
+        {
+            labelGameOver.Visible = false;
+            if(btnArray != null)
+            {
+                foreach (MineSweeperButton btn in btnArray)
+                {
+                    this.Controls.Remove(btn);
+                }
+            }
+            btnArray = null;
+        }
+        //////////////////////////////////Clear Form
+        ////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////Fill form with buttons
+        private void SetUpGrid()
+        {
+            btnArray = new MineSweeperButton[gridSize, gridSize];
+            int btnHeight = 20,
+                btnWidth = 20;
+            for (int i = 0; i < gridSize; i++)
+            {
+                for (int j = 0; j < gridSize; j++)
+                {
+                    MineSweeperButton newBtn = new MineSweeperButton();
+                    btnArray[i, j] = newBtn;
+                    btnArray[i, j].Height = btnHeight;
+                    btnArray[i, j].Width = btnWidth;
+                    btnArray[i, j].Location = new Point(20 * i + 30, 20 * j + 30);
+                    this.Controls.Add(btnArray[i, j]);
+                    newBtn.NeighborNumber = 0;
+                    newBtn.Mine = false;
+                    newBtn.Revealed = false;
+                    newBtn.Flagged = false;
+                    newBtn.Y = i;
+                    newBtn.X = j;
+                    newBtn.Click += button_Click;
+                    newBtn.MouseDown += button_Right_Click;
+                }
+            }
+        }
+        ////////////////////////////////////Fill form with buttons
+        ///////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+        ///////////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////Fill Mines
+        private void FillMines()
+        {
+            Random rnd = new Random();
+            int mineCount = 0;
+            while (mineCount < mineLimit)
+            {
+                int y = rnd.Next(gridSize);
+                int x = rnd.Next(gridSize);
+                if (btnArray[y, x].Mine == true)
+                {
+                    continue;
+                }
+                else
+                {
+                    btnArray[y, x].Mine = true;
+                    mineCount++;
+                }
+            }
+        }
+        ////////////////////////////////////Fill Mines
+        ///////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+        //////////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////Fill Numbers
+        private void FillNumbers()
+        {
+            for (int i = 0; i < gridSize; i++)
+            {
+                for (int j = 0; j < gridSize; j++)
+                {
+                    int neighborCount = 0;
+                    for (int z = 0; z < xNeighborArray.Length; z++)
+                    {
+                        if (i + yNeighborArray[z] < 0 || i + yNeighborArray[z] >= gridSize || j + xNeighborArray[z] < 0 || j + xNeighborArray[z] >= gridSize)
+                        {
+                            continue;
+                        }
+                        if (btnArray[i + yNeighborArray[z], j + xNeighborArray[z]].Mine == true)
+                        {
+                            neighborCount++;
+                        }
+                    }
+                    btnArray[i, j].NeighborNumber = neighborCount;
+                }
+            }
+        }
+        ////////////////////////////////////Fill Numbers
+        /////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+        ///////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////Button left click
+        private void button_Click(object sender, EventArgs e)
+        {
+            MineSweeperButton btn = sender as MineSweeperButton;
+            if(btn.Revealed == false && btn.Flagged == false)
+            {
+                if(btn.Mine == true)
+                {
+                    gameOver = true;
+                    labelGameOver.Text = "You Lost!";
+                    labelGameOver.ForeColor = Color.Red;
+                    labelGameOver.Visible = true;
+                    for(int i = 0; i < gridSize; i++)
+                    {
+                        for(int j = 0; j < gridSize; j++)
+                        {
+                            btnArray[i, j].Enabled = false;
+                            btnArray[i, j].BackgroundImage = null;
+                            if (btnArray[i, j].Mine == true)
+                            {
+                                btnArray[i, j].Text = "*";
+                                btnArray[i, j].BackColor = Color.Red;
+                            }
+                            else if (btnArray[i, j].NeighborNumber == 0 && btnArray[i, j].Mine == false)
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                btnArray[i, j].Text = Convert.ToString(btnArray[i, j].NeighborNumber);
+                            }
+                        }
+                    }
+                }
+                else if(btn.NeighborNumber != 0)
+                {
+                    btn.Enabled = false;
+                    btn.Revealed = true;
+                    btn.Text = Convert.ToString(btn.NeighborNumber);
+                    CheckWin();
+                }
+                else
+                {
+                    int y = btn.Y;
+                    int x = btn.X;
+                    RevealButtons(y, x);
+                    CheckWin();
+                }
+            }
+        }
+        /////////////////////////////////////Button left click
+        ///////////////////////////////////////////////////////////////////////////////////////
+        
+
+
+
+        ///////////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////Reveal Buttons
+        private void RevealButtons(int y, int x)
+        {
+            btnArray[y, x].Enabled = false;
+            btnArray[y, x].Revealed = true;
+            for (int z = 0; z < xNeighborArray.Length; z++)
+            {
+                if (y + yNeighborArray[z] < 0 || y + yNeighborArray[z] >= gridSize || x + xNeighborArray[z] < 0 || x + xNeighborArray[z] >= gridSize)
+                {
+                    continue;
+                }
+                else if(btnArray[y + yNeighborArray[z], x + xNeighborArray[z]].Revealed == true || btnArray[y + yNeighborArray[z], x + xNeighborArray[z]].Flagged == true)
+                {
+                    continue;
+                }
+
+                else
+                {
+                    btnArray[y + yNeighborArray[z], x + xNeighborArray[z]].Enabled = false;
+                    btnArray[y + yNeighborArray[z], x + xNeighborArray[z]].Revealed = true;
+                    if (btnArray[y + yNeighborArray[z], x + xNeighborArray[z]].NeighborNumber != 0)
+                    {
+                        btnArray[y + yNeighborArray[z], x + xNeighborArray[z]].Text = Convert.ToString(btnArray[y + yNeighborArray[z], x + xNeighborArray[z]].NeighborNumber);
+                    }
+                    else
+                    {
+                        RevealButtons(y + yNeighborArray[z], x + xNeighborArray[z]);
+                    }
+                }
+            }
+        }
+        //////////////////////////////////////////Reveal Buttons
+        ////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+        //////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////Button right click
+        private void button_Right_Click(object sender, MouseEventArgs e)
+        {
+            int flagCount;
+            MineSweeperButton btn = (MineSweeperButton)sender;
+            if (e.Button == MouseButtons.Right)
+            {
+                if(btn.Flagged == false)
+                {
+                    flagCount = int.Parse(textBoxFlags.Text);
+                    if (flagCount > 0)
+                    {
+                        btn.Flagged = true;
+                        btn.BackgroundImage = System.Drawing.Image.FromFile("Minesweeper_flag3.png");
+                        flagCount--;
+                        textBoxFlags.Text = Convert.ToString(flagCount);
+                        CheckFlagWin();
+                    }
+                }
+                else
+                {
+                    btn.Flagged = false;
+                    btn.BackgroundImage = null;
+                    flagCount = int.Parse(textBoxFlags.Text);
+                    flagCount++;
+                    textBoxFlags.Text = Convert.ToString(flagCount);
+                }
+            }
+        }
+        /////////////////////////////////////Button right click
+        //////////////////////////////////////////////////////////////////////////////////////
+        
+
+
+        //////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////Check for reveal win
+        private void CheckWin()
+        {
+            int remaining = 0;
+            for(int i = 0; i < gridSize; i++)
+            {
+                for(int j = 0; j < gridSize; j++)
+                {
+                    if(btnArray[i,j].Revealed == false)
+                    {
+                        remaining++;
+                    }
+                }
+            }
+            if(remaining == flagLimit)
+            {
+                gameOver = true;
+                labelGameOver.Text = "You Won!";
+                labelGameOver.ForeColor = Color.Blue;
+                labelGameOver.Visible = true;
+                for(int i = 0; i < gridSize; i++)
+                {
+                    for(int j = 0; j < gridSize; j++)
+                    {
+                        btnArray[i, j].Enabled = false;
+                        if(btnArray[i,j].Mine == true)
+                        {
+                            btnArray[i, j].Text = "*";
+                            btnArray[i, j].BackColor = Color.Green;
+                        }
+                    }
+                }
+            }
+
+        }
+        //////////////////////////////////////Check for reveal win
+        //////////////////////////////////////////////////////////////////////////////////////
+        
+
+        //////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////Check for flag win
+        private void CheckFlagWin()
+        {
+            bool win = true;
+            int flagCount = int.Parse(textBoxFlags.Text);
+            if(flagCount == 0)
+            {
+                for(int i = 0; i < gridSize; i++)
+                {
+                    for (int j = 0; j < gridSize; j++)
+                    {
+                        if (btnArray[i, j].Flagged == true)
+                        {
+                            if(btnArray[i,j].Mine == false)
+                            {
+                                win = false;
+                            }
+                        }
+                    }
+                }
+                if(win == true)
+                {
+                    gameOver = true;
+                    labelGameOver.Text = "You Won!";
+                    labelGameOver.ForeColor = Color.Blue;
+                    labelGameOver.Visible = true;
+                    for(int i = 0; i < gridSize; i++)
+                    {
+                        for(int j = 0; j < gridSize; j++)
+                        {
+                            btnArray[i, j].Enabled = false;
+                        }
+                    }
+                }
+            }
+        }
+        /////////////////////////Check for flag win
+        ///////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+        /////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////Quit
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        /////////////////////////////////////Quit
+        /////////////////////////////////////////////////////////////////////////////////////
+    }
+}
